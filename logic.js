1779d1605496f1a6d9a748fc758b3c9a99d3a794
blad <64702883+blad-dev@users.noreply.github.com>
2025-09-09 09:59:33 +0200

    Add getComputerChoice with random move picking

--- logic.js
+++ logic.js
@@ -1,2 +1,20 @@
 'use strict';
-console.log('Hello, World!');
+
+function scaledRandomInteger(maxRandom) {
+  return Math.floor(Math.random() * maxRandom);
+}
+
+function getComputerChoice() {
+  const randomOption = scaledRandomInteger(3);
+  switch (randomOption) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+    default:
+      throw RangeError(`Get random value out of range: ${randomOption}`);
+  }
+}
+console.log(getComputerChoice());
